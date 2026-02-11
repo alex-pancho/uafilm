e54d4fe2bff2dd50a8c31d1bb9a551165a0c6481
Oleksandr Panchenko <pan.pan.4it@gmail.com>
2026-02-11 16:19:18 +0200

    tiny

--- makeonefile.ps1
+++ makeonefile.ps1
@@ -3,5 +3,5 @@ pyinstaller --onefile --windowed `
   --add-data "miniapp/templates;miniapp/templates" `
   --add-data "miniapp/static;miniapp/static" `
   --add-data "database/films.sqlite;database" `
-  --add-data "icons8-dale-96.ico;." ^
+  --add-data "icons8-dale-96.ico;." `
   index.py
